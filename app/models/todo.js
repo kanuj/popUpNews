358402ebdab82107ce2d8a1b99b4ab5c8f940cd0
Kanuj Bhatnagar <kanujb@brickred.com>
2012-12-19 18:25:02 +0530

    Initial commit

---
+++ app/models/todo.js
@@ -0,0 +1,30 @@
+var Todo = function () {
+
+  this.defineProperties({
+    title: {type: 'string', required: true},
+    status: {type: 'string'},
+  });
+
+  this.validatesPresent('title');
+  this.validatesLength('title', {min: 5});
+
+  this.validatesWithFunction('status', function (status) {
+    return status == 'open' || status == 'done';
+  });
+
+};
+
+/*
+// Can also define them on the prototype
+Todo.prototype.someOtherMethod = function () {
+  // Do some other stuff
+};
+// Can also define static methods and properties
+Todo.someStaticMethod = function () {
+  // Do some other stuff
+};
+Todo.someStaticProperty = 'YYZ';
+*/
+
+Todo = geddy.model.register('Todo', Todo);
+
